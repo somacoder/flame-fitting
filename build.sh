f7140cb76b2fabd0aac8d6c5505b1a29f24d9fd3
somacoder <gkbartleyjr@gmail.com>
2022-11-17 17:33:11 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda deactivate
+conda env remove -n flame-fitting
+conda env create -f environment.yml
+conda activate flame-fitting
+
 cp /mnt/e/3rdParty/flame-fitting/models/* models
 cd sbody/alignment/mesh_distance
 make
